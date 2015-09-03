621e81e4b8e95963ca90092e83321f1511ce8bb4
Joe Lambert <joe@joelambert.co.uk>
2015-09-03 14:52:44 +0100

    Initial Commit

---
+++ src/Twig/Template.php
@@ -0,0 +1,41 @@
+<?php namespace Rareloop\Primer\TemplateEngine\Twig;
+
+use Rareloop\Primer\Templating\Template as PrimerTemplate;
+use Rareloop\Primer\TemplateEngine\Twig\Twig;
+use Rareloop\Primer\Primer;
+use Rareloop\Primer\Templating\ViewData;
+
+
+class Template extends PrimerTemplate
+{
+    /**
+     * Array of file extensions
+     *
+     * @var array
+     */
+    protected $extension = 'twig';
+
+    /**
+     * Render this template with the provided data
+     *
+     * @param  ViewData $data [description]
+     * @return [type]       [description]
+     */
+    public function render($data = null)
+    {
+        // Ensure that $data is not null
+        if(!isset($data)) {
+            $data = new ViewData(array());
+        }
+
+        // Access the singleton Twig engine
+        $engine = Twig::instance();
+
+        // The Twig loader is setup to load from the Primer base path so we need to remove this
+        // from the template path so that we have it relative to the base
+        $path = str_replace(Primer::$BASE_PATH, '', $this->directory);
+
+        // Render the template
+        return $engine->render($path . '/'. $this->filename . '.' . $this->extension, $data->toArray());
+    }
+}
