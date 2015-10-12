c90147595f8778c33755f9340635e74fd654ca1d
Joe Lambert <joe@joelambert.co.uk>
2015-10-12 20:21:22 +0100

    Added  function to get a template string

--- src/Twig/Template.php
+++ src/Twig/Template.php
@@ -5,7 +5,6 @@ use Rareloop\Primer\TemplateEngine\Twig\Twig;
 use Rareloop\Primer\Primer;
 use Rareloop\Primer\Templating\ViewData;
 
-
 class Template extends PrimerTemplate
 {
     /**
@@ -24,18 +23,46 @@ class Template extends PrimerTemplate
     public function render($data = null)
     {
         // Ensure that $data is not null
-        if(!isset($data)) {
+        if (!isset($data)) {
             $data = new ViewData(array());
         }
 
         // Access the singleton Twig engine
         $engine = Twig::instance();
 
-        // The Twig loader is setup to load from the Primer base path so we need to remove this
-        // from the template path so that we have it relative to the base
-        $path = str_replace(Primer::$BASE_PATH, '', $this->directory);
-
         // Render the template
-        return $engine->render($path . '/'. $this->filename . '.' . self::$extension, $data->toArray());
+        return $engine->render($this->templatePath(), $data->toArray());
+    }
+
+    /**
+     * Get the raw template data
+     *
+     * @return String The template before compilation
+     */
+    public function raw()
+    {
+        // Access the singleton Twig engine
+        $engine = Twig::instance();
+
+        return $engine->getLoader()->getSource($this->templatePath());
+    }
+
+    /**
+     * Returns the path to the requested template file
+     *
+     * @return String Filepath
+     */
+    protected function templatePath()
+    {
+        if (isset($this->filename)) {
+            // The Twig loader is setup to load from the Primer base path so we need to remove this
+            // from the template path so that we have it relative to the base
+            $path = str_replace(Primer::$BASE_PATH, '', $this->directory);
+
+            // Render the template
+            return $path . '/'. $this->filename . '.' . self::$extension;
+        } else {
+            throw new \Exception('Attempting to get template path before loading a template');
+        }
     }
 }
